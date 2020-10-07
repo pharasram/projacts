84aed7c70780ceaf6b9c69fe0a1aa7de5827c3e2
parsuram <pharashramrai@gmail.com>
2020-10-07 01:33:16 -0700

    modified tamplates to display Posts from database

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.Post_list, name='Post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+from django.utils import timezone
+from .models import Post
+
+def Post_list(request):
+    Posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/Post_list.html', {'Posts': Posts})
